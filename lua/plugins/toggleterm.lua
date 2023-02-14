58ea83fe5ae51d40a4782577887efe460fe866d2
”Jacob <jacob@jberglund.se>
2023-02-14 08:27:43 +0100

    Mergey-merge

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,18 +2,30 @@
 return {
 	"akinsho/toggleterm.nvim",
 	version = "*",
-	keys = {
-		{ "<leader>Tt", "<cmd>ToggleTerm<cr>", desc = "ToggleTerm" },
-	},
 	cmd = "ToggleTerm",
+	keys = {
+		{ "<c-/>", "<cmd>ToggleTerm<cr>", desc = "ToggleTerm" },
+	},
 	config = function()
 		require("toggleterm").setup({
-			-- direction = "float",
-			-- float_opts = {
-			-- 	border = "single",
-			-- 	width = 100,
-			-- 	height = 30,
-			-- },
+			direction = "float",
+			float_opts = {
+				border = "single",
+			},
+			highlights = {
+				-- highlights which map to a highlight group name and a table of it's values
+				-- NOTE: this is only a subset of values, any group placed here will be set for the terminal window split
+				-- Normal = {
+				-- 	guibg = "<VALUE-HERE>",
+				-- },
+				-- NormalFloat = {
+				-- 	link = "Normal",
+				-- },
+				FloatBorder = {
+					guifg = "green",
+					guibg = "green",
+				},
+			},
 		})
 	end,
 }
